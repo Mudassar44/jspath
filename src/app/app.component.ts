5decb162de7d0a844da3ccb2dec3907117056032
Mudassar Mustafai <mudassarmustafai@hotmail.com>
2022-03-22 01:22:40 +0500

    destructuring variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,43 @@ export class AppComponent implements AfterViewInit {
    */
   
   constructor() {
-  
+    this.declaringVariable();
+  }
+
+  declaringVariable() {
+    //Declaring different variables with different methods.
+    var number : Number | undefined = 9;
+    let name = 'JsPath';
+    //Destructuring assignments.
+    let a, b, rest;
+    [a, b] = [10, 20];
+    console.log(a); // 10
+    console.log(b); // 20
+    
+    [a, b, ...rest] = [10, 20, 30, 40, 50];
+    console.log(a); // 10
+    console.log(b); // 20
+    console.log(rest); // [30, 40, 50]
+    
+    ({ a, b } = { a: 10, b: 20 });
+    console.log(a); // 10
+    console.log(b); // 20
+    
+    // Stage 4(finished) proposal
+    ({a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40});
+    console.log(a); // 10
+    console.log(b); // 20
+    console.log(rest); // {c: 30, d: 40}
+
+    // let name2, name3, name4 , othersName;
+    // ({name2,name3,name4,othersName} = {name2: 'My Name2' , name3: 'My Name 3', name4 : 'My Name 4', othersName : 'My Others Name'}); 
+    
+    //Array Destructuring
+    const foo = ['one','two','three'];
+    const [red,yellow,green] = foo;
+    console.log(red);
+    console.log(yellow);
+    console.log(green);
   }
 
   ngAfterViewInit() {
